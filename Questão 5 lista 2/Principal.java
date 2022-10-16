0a497424fd446c975f0e0d419657b20626591943
RichardMatheus929 <89545652+RichardMatheus929@users.noreply.github.com>
2022-10-16 12:26:55 -0300

    Create Principal.java

---
+++ Questão 5 lista 2/Principal.java
@@ -0,0 +1,52 @@
+package Visão;
+
+import java.awt.Rectangle;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) throws IOException {
+	
+		Retangulo ra = new Retangulo();
+		Retangulo rb = new Retangulo();
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Diga o caminho do seu arquivo");
+		String caminho = ler.nextLine();
+		Scanner a = new Scanner(new FileReader(caminho));
+		
+		ra.setA(a.nextInt());
+		ra.setL(a.nextInt());
+		ra.setX(a.nextInt());
+		ra.setY(a.nextInt());
+		rb.setA(a.nextInt());
+		rb.setL(a.nextInt());
+		rb.setX(a.nextInt());
+		rb.setY(a.nextInt());
+	
+		Rectangle rectOne = new Rectangle (ra.getA(), ra.getL(), ra.getX(), ra.getY());
+		Rectangle rectTwo = new Rectangle (rb.getA(), rb.getL() ,rb.getX(), rb.getY());
+		boolean x = rectOne.intersects ( rectTwo );
+		
+		if(x) {
+			System.out.print("O seu retângulo ("+ra.getA()+" "+ra.getL()+" "+ra.getX()+" "+ra.getY()+") ");
+			System.out.println("Colidiu com o retângulo");
+			System.out.println("( "+ra.getA()+" "+ra.getL()+" "+ra.getX()+" "+ra.getY()+" )");
+		} else {
+			System.out.print("O seu retângulo ("+ra.getA()+" "+ra.getL()+" "+ra.getX()+" "+ra.getY()+") ");
+			System.out.println("Não colidiu com o retângulo");
+			System.out.println("( "+rb.getA()+" "+rb.getL()+" "+rb.getX()+" "+rb.getY()+" )");
+		}
+		
+		
+				
+	}
+	
+	
+
+}
