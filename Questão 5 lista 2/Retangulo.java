48735bf275a60333953cb9a4c90ffe7e46829c13
RichardMatheus929 <89545652+RichardMatheus929@users.noreply.github.com>
2022-10-16 12:27:46 -0300

    Create Retangulo.java

---
+++ Questão 5 lista 2/Retangulo.java
@@ -0,0 +1,38 @@
+package Visão;
+
+public class Retangulo {
+	
+	public int a;
+	public int l;
+	public int x;
+	public int y;
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getL() {
+		return l;
+	}
+	public void setL(int l) {
+		this.l = l;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+	
+
+}
