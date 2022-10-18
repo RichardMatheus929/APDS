085cdb3dcb40bdc5c90ec667c99c1c2f2578afd0
RichardMatheus929 <89545652+RichardMatheus929@users.noreply.github.com>
2022-10-18 00:32:11 -0300

    Create Principal.java

---
+++ Colisões/Principal.java
@@ -0,0 +1,19 @@
+package Visão;
+
+import java.awt.Rectangle;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) throws IOException {
+		Retangulo r = new Retangulo();
+		r.ler();
+
+	}
+
+}
