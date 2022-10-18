7102bcb75b1401117c240d7bb410f15ae95f95b3
RichardMatheus929 <89545652+RichardMatheus929@users.noreply.github.com>
2022-10-18 00:49:16 -0300

    Create Pessoa.java

---
+++ calcular tempo de emprego/Pessoa.java
@@ -0,0 +1,66 @@
+package visão;
+import java.util.Date;
+
+public class Pessoa {
+	
+	public String nome;
+	public int diaa;
+	public int mesa;
+	public int anoa;
+	public int diab;
+	public int mesb;
+	public int anob;
+	public String empresa;
+	public int getDiaa() {
+		return diaa;
+	}
+	public void setDiaa(int diaa) {
+		this.diaa = diaa;
+	}
+	public int getMesa() {
+		return mesa;
+	}
+	public void setMesa(int mesa) {
+		this.mesa = mesa;
+	}
+	public int getAnoa() {
+		return anoa;
+	}
+	public void setAnoa(int anoa) {
+		this.anoa = anoa;
+	}
+	public int getDiab() {
+		return diab;
+	}
+	public void setDiab(int diab) {
+		this.diab = diab;
+	}
+	public int getMesb() {
+		return mesb;
+	}
+	public void setMesb(int mesb) {
+		this.mesb = mesb;
+	}
+	public int getAnob() {
+		return anob;
+	}
+	public void setAnob(int anob) {
+		this.anob = anob;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+
+}
