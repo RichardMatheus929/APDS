b4edf6814e3c2941f352c3aac9910b111126bf73
RichardMatheus929 <89545652+RichardMatheus929@users.noreply.github.com>
2022-10-18 00:48:35 -0300

    Create Principal.java

---
+++ calcular tempo de emprego/Principal.java
@@ -0,0 +1,27 @@
+package visão;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+		int op = 1;
+		do {
+			Scanner ler = new Scanner(System.in);
+			Calcular c = new Calcular();
+			System.out.println("Bem vindo!");
+			System.out.println("1 - Calcular tempo");
+			System.out.println("2 - sair");
+			op = ler.nextInt();
+			
+			switch(op) {
+			case 1: c.calcula();
+				break;
+			case 2: c.fim();
+				break;
+		} }while(op != 2);
+	}
+}
